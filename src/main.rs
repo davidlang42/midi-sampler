1ee2754f5b78234f3dc688bf0e83eb975f343c3a
David Lang <davidlang42@gmail.com>
2024-03-10 12:59:44 +1100

    Simple sound test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::OutputStream;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Getting stream...");
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    println!("Loading file...");
+    let file = BufReader::new(File::open("gong.wav").unwrap());
+    println!("Playing sound...");
+    let sink = stream_handle.play_once(file).unwrap();
+    println!("Waiting for finish...");
+    sink.sleep_until_end();
+    println!("Finished.");
 }
